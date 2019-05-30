a445955ca03d4fc1176c8df1dd4eb48f58ce254f
Anton Sustavov <sustav86@gmail.com>
2019-05-30 14:02:02 +0300

    implements filters

--- src/main/java/jaas/JaasFilter.java
+++ src/main/java/jaas/JaasFilter.java
@@ -1,7 +1,10 @@
 package jaas;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
 
 public class JaasFilter implements Filter {
     @Override
@@ -12,11 +15,44 @@ public class JaasFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        // Если в реквесте уже есть Principal - ничего не делаем
+        if (req.getUserPrincipal() != null ) {
+            chain.doFilter(request, response);
+        }
 
+        // Получаем security Header. А если его нет - запрашиваем
+        String secHeader = req.getHeader("authorization");
+        if (secHeader == null) {
+            requestNewAuthInResponse(response);
+        }// Проверяем аутентификацию
+        else {
+            String authorization = secHeader.replace("Basic ", "");
+            Base64.Decoder decoder = java.util.Base64.getDecoder();
+            authorization = new String(decoder.decode(authorization));
+            String[] loginData = authorization.split(":");
+            try {
+                if (loginData.length == 2) {
+                    req.login(loginData[0], loginData[1]);
+                    chain.doFilter(request, response);
+                } else {
+                    requestNewAuthInResponse(response);
+                }
+            } catch (ServletException e) {
+                requestNewAuthInResponse(response);
+            }
+        }
     }
 
     @Override
     public void destroy() {
 
+    }
+
+    private void requestNewAuthInResponse(ServletResponse response) throws IOException {
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String value = "Basic realm=\"JaasLogin\"";
+        resp.setHeader("WWW-Authenticate", value);
+        resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
